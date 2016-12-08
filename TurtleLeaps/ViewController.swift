d7b04b84b1d337cf62526194952297fd09309b06
habi-kvana <kvanadev1@gmail.com>
2016-12-08 11:42:02 +0530

    first commit

---
+++ TurtleLeaps/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TurtleLeaps
+//
+//  Created by kvanadev5 on 08/12/16.
+//  Copyright © 2016 kvanadev5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        /*world kvana*/
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
